07c1330d7e9c49fa4de5e9604cadbfcf4e5ca3a4
Bandari Sandeep <107421940+Bandarisandeep26@users.noreply.github.com>
2025-07-28 21:56:17 +0530

    Update variables.tf
    add some changes

--- variables.tf
+++ variables.tf
@@ -1,42 +1,49 @@
-variable "instance_count" {
-  type        = number
-  default     = 1
-}
-
 variable "bucket_count" {
+  description = "Number of S3 buckets to create"
   type        = number
   default     = 1
 }
 
 variable "bucket_name_prefix" {
+  description = "Prefix for S3 buckets"
   type        = string
   default     = "my-demo-bucket"
 }
 
+variable "instance_count" {
+  description = "Number of EC2 instances to create"
+  type        = number
+  default     = 1
+}
 
 variable "ami_id" {
+  description = "AMI ID to launch"
   type        = string
-  description = "AMI ID"
 }
 
 variable "instance_type" {
+  description = "EC2 instance type"
   type        = string
   default     = "t2.micro"
 }
 
 variable "subnet_id" {
+  description = "Subnet ID for the EC2 instance"
   type        = string
 }
 
 variable "security_group_ids" {
+  description = "List of security group IDs"
   type        = list(string)
 }
 
 variable "key_name" {
+  description = "EC2 Key Pair name"
   type        = string
 }
 
 variable "tags" {
+  description = "Tags to apply"
   type        = map(string)
   default     = {}
 }
