ec6a6e54af7e949fb867ec82ff2244a6fb6467e2
Bandari Sandeep <107421940+Bandarisandeep26@users.noreply.github.com>
2025-07-28 21:58:11 +0530

    Create outputs.tf
    add outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,11 @@
+output "bucket_names" {
+  value = module.s3.bucket_names
+}
+
+output "ec2_instance_ids" {
+  value = module.ec2.instance_ids
+}
+
+output "ec2_public_ips" {
+  value = module.ec2.public_ips
+}
